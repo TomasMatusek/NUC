9d41224d8d3d8a8db4cdc59507e5ed1b8b486cb4
Tomáš Matúšek <matusek@foundation.sk>
2025-10-07 16:00:32 +0200

    Simple hellow world project

---
+++ nuc-web-app-spring-service/nuc-web-app-spring-service/src/main/java/online/ultimate_homelab/nuc_web_app_spring_service/NucWebAppSpringServiceApplication.java
@@ -0,0 +1,22 @@
+package online.ultimate_homelab.nuc_web_app_spring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class NucWebAppSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucWebAppSpringServiceApplication.class, args);
+	}
+
+	@RestController
+    static class NucWebAppController {
+		@GetMapping(path = "/")
+		public String test() {
+			return "Hello World";
+		}
+	}
+}
