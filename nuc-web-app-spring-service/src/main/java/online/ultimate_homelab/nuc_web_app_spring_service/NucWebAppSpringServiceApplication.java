ba27aa2fb754f86f13865b0aa2477a07d3e03599
Tomas Matusek <tmatusek@datto.com>
2025-10-14 21:22:22 +0200

    Test env var

--- nuc-web-app-spring-service/src/main/java/online/ultimate_homelab/nuc_web_app_spring_service/NucWebAppSpringServiceApplication.java
+++ nuc-web-app-spring-service/src/main/java/online/ultimate_homelab/nuc_web_app_spring_service/NucWebAppSpringServiceApplication.java
@@ -1,5 +1,6 @@
 package online.ultimate_homelab.nuc_web_app_spring_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class NucWebAppSpringServiceApplication {
 
+	@Value("${MY_ENVIRONMENT_VARIABLE:default-value}")
+    private String myEnvVar;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NucWebAppSpringServiceApplication.class, args);
 	}
 
 	@RestController
-    static class NucWebAppController {
+    class NucWebAppController {
 		@GetMapping(path = "/")
 		public String test() {
-			return "Hello World from Spring service";
+			return "Hello World from Spring service: " + myEnvVar;
 		}
 	}
 }
