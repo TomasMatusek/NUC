3223b8a92a481629f193b3e7832e0674ed5a9773
Tomáš Matúšek <matusek@foundation.sk>
2025-10-22 16:15:09 +0200

    Adjust workflow spring

--- apps/spring-service/src/main/java/online/ultimate_homelab/nuc_web_app_spring_service/NucController.java
+++ apps/spring-service/src/main/java/online/ultimate_homelab/nuc_web_app_spring_service/NucController.java
@@ -3,6 +3,7 @@ package online.ultimate_homelab.nuc_web_app_spring_service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 class NucController {
@@ -13,13 +14,23 @@ class NucController {
     @Value("${VERSION:unknown}")
     private String version;
 
+    @Value("${ENVIRONMENT:unknown}")
+    private String environment;
+
+    @Value("${COMMIT:unknown}")
+    private String commit;
+
     @GetMapping("/")
     public String test() {
         return "Hello World from Spring service: " + postgresUser;
     }
 
     @GetMapping("/version")
-    public String getVersion() {
-        return "Version: " + version;
-    }
+public Map<String, String> getVersion() {
+    Map<String, String> response = new HashMap<>();
+    response.put("version", version);
+    response.put("commit", commit);
+    response.put("environment", environment);
+    return response;
+}
 }
