9c94c88d25aa2d43ddaff89e593642759f051064
Tomáš Matúšek <matusek@foundation.sk>
2025-11-11 13:51:44 +0100

    Controller changes

---
+++ apps/spring-service/src/main/java/online/ultimate_homelab/nuc_web_app_spring_service/DataSourceConfig.java
@@ -0,0 +1,18 @@
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://postgres:5432/mydb")
+                .username("myuser")
+                .password("mypassword")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
